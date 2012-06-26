6f9ddac7b83fc17ede743ee6e809e42f95b839c6
jim huang <jim.javathunderbird@gmail.com>
2012-06-26 09:45:57 -0700

    hook up to phpnativejade

--- src/Pupcake/ResponseRenderJade/Main.php
+++ src/Pupcake/ResponseRenderJade/Main.php
@@ -5,28 +5,24 @@
 namespace Pupcake\ResponseRenderJade;
 
 use Pupcake;
-use Everzet;
+use PHPNativeJade\Renderer;
 
 class Main extends Pupcake\plugin
 {
     public function load($config = array())
     {
+        if(!isset($config['jade_compiler'])){
+            throw new ConfigurationException("Missing Jade compiler, Please set jade_compiler in the configuration!");
+        }
+
         $this->on("pupcake.responserender.render.start", function($event) use ($config) {
             $view_engine = strtolower($event->props('view_engine'));
             if($view_engine == "jade"){
                 $path_info = pathinfo($event->props('view_path'));
                 if($path_info['extension'] == 'jade'){
                     //now everything is good, start rendering
-                    $dumper = new Everzet\Jade\Dumper\PHPDumper();
-                    $dumper->registerVisitor('tag', new Everzet\Jade\Visitor\AutotagsVisitor());
-                    $dumper->registerFilter('javascript', new Everzet\Jade\Filter\JavaScriptFilter());
-                    $dumper->registerFilter('cdata', new Everzet\Jade\Filter\CDATAFilter());
-                    $dumper->registerFilter('php', new Everzet\Jade\Filter\PHPFilter());
-                    $dumper->registerFilter('style', new Everzet\Jade\Filter\CSSFilter());
 
-                    $parser = new Everzet\Jade\Parser(new Everzet\Jade\Lexer\Lexer());
-                    $jade   = new Everzet\Jade\Jade($parser, $dumper);
-
+                    $renderer = new PHPNativeJade\Renderer();
                     $view_path = $event->props('view_path');
                     $view_diretory = $event->props('view_directory');
                     if(strlen($view_diretory) > 0){
@@ -36,13 +32,12 @@ class Main extends Pupcake\plugin
                     $view_cache_enabled = $event->props('view_cache_enabled');
 
                     if(!$view_cache_enabled){
-                        $output = $jade->render($view_path);
-                        file_put_contents($view_path.".php", $output);
+                        $jade->render($view_path);
                     }
 
-                    if(is_readable($view_path.".php")){
-                        extract($event->props('data'));
-                        require $view_path.".php";
+                    $cache_template = $path_info['filename'].".html";
+                    if(is_readable($cache_template)){
+                        require $cache_template;
                     }
                 }
             }
