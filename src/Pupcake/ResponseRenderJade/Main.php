cab396f7eaa8e395bdbbb7f08aded405abe43f2e
jim huang <jim.javathunderbird@gmail.com>
2012-06-26 22:26:03 -0700

    if no jade compiler is set, try to find the default one

--- src/Pupcake/ResponseRenderJade/Main.php
+++ src/Pupcake/ResponseRenderJade/Main.php
@@ -10,21 +10,16 @@ class Main extends Pupcake\plugin
 {
     public function load($config = array())
     {
-        if(!isset($config['jade_compiler'])){
-            throw new ConfigurationException("Missing Jade compiler, Please set jade_compiler in the configuration!");
-        }
-
         $this->on("pupcake.responserender.render.start", function($event) use ($config) {
             $view_engine = strtolower($event->props('view_engine'));
             if($view_engine == "jade"){
                 $path_info = pathinfo($event->props('view_path'));
                 if($path_info['extension'] == 'jade'){
-                    if(!isset($config['jade_compiler'])){
-                        throw new ConfigurationException("Missing jade compiler, please set jade_compiler in configuration!");
-                    }
                     //now everything is good, start rendering
                     $renderer = new \PHPNativeJade\Renderer();
-                    $renderer->setNativeJadeCompiler($config['jade_compiler']);
+                    if(isset($config['jade_compiler'])){
+                        $renderer->setNativeJadeCompiler($config['jade_compiler']);
+                    }
                     $view_path = $event->props('view_path');
                     $view_diretory = $event->props('view_directory');
                     if(strlen($view_diretory) > 0){
